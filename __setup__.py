8aa9b9c015086ad0064c498050258707a8b827c9
Ivan Latunov <xswordsx@gmail.com>
2013-07-04 18:16:43 +0300

    Update version of __setup__.py

--- __setup__.py
+++ __setup__.py
@@ -1,6 +1,6 @@
 from cx_Freeze import setup, Executable
 
 setup(name = "Python Cthulhu",
-        version = "0.1",
+        version = "0.2",
         description = "I wish, I wish a nigga would",
         executables = [Executable("RPG-main.py")])
