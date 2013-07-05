514b4e857224a3996f26c7fbd7b2d419e5a31675
Ivan Latunov <xswordsx@gmail.com>
2013-07-05 10:55:45 +0300

    Update 'Stats.py'
    Add agility

--- Stats.py
+++ Stats.py
@@ -4,9 +4,10 @@ import pygame
 # Player and NPC stats
 class Stats:
 
-    def __init__(self, max_hp, health, strength, level, xp):
+    def __init__(self, max_hp=65, strength=100, agility=100, level=1, xp=0):
         self.max_hp = max_hp
-        self.health = health
-        self.stregth = stregth
+        self.current_hp = max_hp
+        self.strength = strength
+        self.agility = agility
         self.level = level
         self.xp = xp
